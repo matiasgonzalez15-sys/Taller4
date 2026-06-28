15951d6588d6b3b9c3d8f3420dfee512e9602a37
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-28 13:24:10 -0400

    Update OrdenarPoder.java

--- src/Strategy/OrdenarPoder.java
+++ src/Strategy/OrdenarPoder.java
@@ -1,15 +1,29 @@
 package Strategy;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import Dominio.Carta;
 
 public class OrdenarPoder implements EstrategiaOrdenar {
+	
+
+	public OrdenarPoder() {
+	}
 
 	@Override
 	public List<Carta> ordenar(List<Carta> cartas) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Carta> copia = new LinkedList<Carta>(cartas);
+		for(int a = 0; a < copia.size() - 1; a++) {
+			for(int b = a+1; b < copia.size(); b++) {
+				if(copia.get(a).getPoder() < copia.get(b).getPoder()) {
+					Carta temp = copia.get(a);
+					copia.set(a, copia.get(b));
+					copia.set(b, temp);
+				}
+			}
+		}
+		return copia;
 	}
 
 }

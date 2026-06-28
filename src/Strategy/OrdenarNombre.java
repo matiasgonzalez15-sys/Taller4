dbd31c0d34a2963d01ffdb86395e1be2394bc9b1
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-28 13:23:28 -0400

    Add sorting implementation in OrdenarNombre class
    Implement sorting logic for Carta objects by name.

--- src/Strategy/OrdenarNombre.java
+++ src/Strategy/OrdenarNombre.java
@@ -1,15 +1,31 @@
 package Strategy;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import Dominio.Carta;
 
 public class OrdenarNombre implements EstrategiaOrdenar {
+	
+
+	public OrdenarNombre() {
+	}
 
 	@Override
 	public List<Carta> ordenar(List<Carta> cartas) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Carta> copia = new LinkedList<Carta>(cartas);
+		for(int a = 0; a < copia.size() - 1; a++) {
+			for(int b = a+1; b < copia.size(); b++) {
+				char c1 = Character.toLowerCase(copia.get(a).getNombre().charAt(0));
+				char c2 = Character.toLowerCase(copia.get(b).getNombre().charAt(0));
+				if(c1 < c2) {
+					Carta temp = copia.get(a);
+					copia.set(a, copia.get(b));
+					copia.set(b, temp);
+				}
+			}
+		}
+		return copia;
 	}
 
 }

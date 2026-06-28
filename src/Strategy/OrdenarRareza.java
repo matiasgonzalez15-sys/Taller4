11e20cf41ff0b9dee5331f1f13b625e3985a4bb4
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-28 13:24:32 -0400

    Add sorting by rarity in OrdenarRareza class
    Implemented sorting logic for cartas based on rarity.

--- src/Strategy/OrdenarRareza.java
+++ src/Strategy/OrdenarRareza.java
@@ -1,15 +1,28 @@
 package Strategy;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import Dominio.Carta;
 
 public class OrdenarRareza implements EstrategiaOrdenar {
+	
+	public OrdenarRareza() {
+	}
 
 	@Override
 	public List<Carta> ordenar(List<Carta> cartas) {
-		List<Carta> copia = cartas;
-		return null;
+		List<Carta> copia = new LinkedList<Carta>(cartas);
+		for(int a = 0; a < copia.size() - 1; a++) {
+			for(int b = a+1; b < copia.size(); b++) {
+				if(copia.get(a).getRareza() < copia.get(b).getRareza()) {
+					Carta temp = copia.get(a);
+					copia.set(a, copia.get(b));
+					copia.set(b, temp);
+				}
+			}
+		}
+		return copia;
 	}
 
 }

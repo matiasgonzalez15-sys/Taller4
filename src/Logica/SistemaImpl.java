ee9ab94c9376a4128a1a86e5e45ce36634118e8c
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-28 13:25:00 -0400

    Implement sorting strategy and extras extraction
    Added strategy method for sorting cards and a method to extract additional information from cards.

--- src/Logica/SistemaImpl.java
+++ src/Logica/SistemaImpl.java
@@ -3,43 +3,63 @@ package Logica;
 import java.util.*;
 
 import Dominio.*;
+import Strategy.EstrategiaOrdenar;
 import Visitor.VisitorCalcularPoder;
 import Visitor.VisitorPoder;
 
 public class SistemaImpl implements Sistema {
 	private List<Carta> cartas;
 	private static SistemaImpl instancia;
-	
-
 
 	private SistemaImpl() {
 		this.cartas = new LinkedList<Carta>();
 	}
-	
-
 
 	public static SistemaImpl getInstancia() {
-		if(instancia == null) {
+		if (instancia == null) {
 			instancia = new SistemaImpl();
 		}
 		return instancia;
 	}
 
-
-
 	@Override
 	public void crearObjetos(String[] partes) {
-		if(partes.length == 0) {
+		if (partes.length == 0) {
 			return;
 		}
 		FactoryCarta factory = new FactoryCarta();
 		Carta c = factory.crear(partes);
-		if(c!= null) {
+		if (c != null) {
 			VisitorPoder visitor = new VisitorCalcularPoder();
 			c.accept(visitor);
 			cartas.add(c);
 		}
-		
+
+	}
+
+	@Override
+	public String estrategia(EstrategiaOrdenar estrategia) {
+		String res = "";
+		List<Carta> listaEstrategia = estrategia.ordenar(cartas);
+		return res;
+	}
+
+	private String sacarExtras(Carta c) {
+		String extras = "";
+		if (c.getClass().getSimpleName().equalsIgnoreCase("pokemon")) {
+			Pokemon p = (Pokemon) c;
+			extras += " | Daño: " + p.getDaño() + " | Cant. Energia: " + p.getCantEnergia();
+		} else if (c.getClass().getSimpleName().equalsIgnoreCase("item")) {
+			Item i = (Item) c;
+			extras += " | Bonificacion: " + i.getBonificacion();
+		} else if (c.getClass().getSimpleName().equalsIgnoreCase("supporter")) {
+			Supporter s = (Supporter) c;
+			extras += " | Efectos Turno: " + s.getEfectosTurno();
+		} else {
+			Energy e = (Energy) c;
+			extras += " | Elemento: " + e.getElemento();
+		}
+		return extras;
 	}
 
 }

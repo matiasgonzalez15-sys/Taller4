175f6db12596bf6448858938479844ecf720b240
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-30 16:24:53 -0400

    Update App.java

--- src/Logica/App.java
+++ src/Logica/App.java
@@ -1,24 +1,35 @@
 package Logica;
 
 import java.io.*;
+import javax.swing.SwingUtilities;
+import GUI.MainGUI;
 import java.util.*;
+
 
 public class App {
 	// Matias Ignacio Gonzalez Gomez 22.350.340-3
 	static SistemaImpl sistema = SistemaImpl.getInstancia();
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws Exception {
 		cargarArchivo();
-
-	}
-	private static void cargarArchivo() throws FileNotFoundException {
-		File arch = new File("Sobres.txt");
-		Scanner lector = new Scanner(arch);
-		while(lector.hasNext()) {
-			String linea = lector.nextLine();
-			String[] partes = linea.split(";");
-			sistema.crearObjetos(partes);
-		}
+		SwingUtilities.invokeLater(() -> new MainGUI());
 		
+	}
+	private static void cargarArchivo() throws Exception {
+	    File arch = new File("Sobres.txt");
+	    Scanner lector = new Scanner(arch);
+	    while (lector.hasNextLine()) {
+	        String linea = lector.nextLine().trim();
+	        if (linea.isEmpty()) {
+	            continue;
+	        }
+	        String[] partes = linea.split(";");
+	        try {
+	            sistema.crearObjetos(partes);
+	        } catch (Exception e) {
+	            System.err.println("Línea inválida, se omite: " + linea + " (" + e.getMessage() + ")");
+	        }
+	    }
+	    lector.close();
 	}
 
 }

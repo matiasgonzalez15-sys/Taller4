f88b51081dd2cb831e013d4d172cb26ea31d6857
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-30 16:25:32 -0400

    Update Sistema.java

--- src/Logica/Sistema.java
+++ src/Logica/Sistema.java
@@ -1,9 +1,17 @@
 package Logica;
 
+import java.io.IOException;
+import java.util.List;
+
+import Dominio.Carta;
 import Strategy.EstrategiaOrdenar;
 
 public interface Sistema {
-	void crearObjetos(String[] partes);
-	String estrategia(EstrategiaOrdenar estrategia);
+	void crearObjetos(String[] partes) throws Exception;
+	List<Carta> estrategia(EstrategiaOrdenar estrategia);
+	List<Carta> getCartas();
+	void eliminarCarta(int indice) throws Exception;
+	void guardarCartas() throws Exception;
+		
 
 }
